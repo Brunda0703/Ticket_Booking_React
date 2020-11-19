1abf7c49046745d4e21368564aaad3f0ed81f473
Brunda0703 <58696156+Brunda0703@users.noreply.github.com>
2020-11-19 11:12:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react'; // eslint-disable-next-lin
-import { BrowserRouter , Route } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Route,
+  } from 'react-router-dom';
 import { Booking } from './components/Booking.js';
 import { Dashboard } from './components/Dashboard.js';
 import './App.css';
@@ -9,10 +12,14 @@ export class App extends Component {
   render() {
 
     return (
-      <BrowserRouter>
+      <Router>
+        <div>
+          <Header />
           <Route path="/" component={Booking} />
           <Route path="/dashboard" component={Dashboard} />
-      </BrowserRouter>
+          <Footer />
+        </div>
+      </Router>
     )
   }
 }
